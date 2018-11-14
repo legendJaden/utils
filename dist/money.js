abbac1242f66b65108772cb93bd5309578fdeb74
林鉴伟 <linjw@ap-ec.cn>
2018-11-14 16:15:22 +0800

    提交人：林鉴伟 提交内容：修改money.js bug（旧方法精度判断太粗糙，太多问题了，换新的）

--- dist/money.js
+++ dist/money.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -7,28 +7,79 @@ exports.default = {
   /* --------------------------- 金额处理类 ----------------------------- */
   /** 数字金额大写转换(可以处理整数,小数,负数) */
   smalltoBIG: function smalltoBIG(n) {
-    var fraction = ['角', '分'];
-    var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
-    var unit = [['元', '万', '亿'], ['', '拾', '佰', '仟']];
-    var head = n < 0 ? '欠' : '';
-    n = Math.abs(n);
+    var cnNums = new Array("零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"); //汉字的数字
+    var cnIntRadice = new Array("", "拾", "佰", "仟"); //基本单位
+    var cnIntUnits = new Array("", "万", "亿", "兆"); //对应整数部分扩展单位
+    var cnDecUnits = new Array("角", "分", "毫", "厘"); //对应小数部分单位
+    var cnInteger = "整"; //整数金额时后面跟的字符
+    var cnIntLast = "元"; //整型完以后的单位
+    var maxNum = 999999999999999.9999; //最大处理的数字
 
-    var s = '';
-
-    for (var i = 0; i < fraction.length; i++) {
-      s += (digit[parseInt(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+    var IntegerNum; //金额整数部分
+    var DecimalNum; //金额小数部分
+    var ChineseStr = ""; //输出的中文金额字符串
+    var parts; //分离金额后用的数组，预定义
+    if (money == "") {
+      return "";
     }
-    s = s || '整';
-    n = Math.floor(n);
-
-    for (var _i = 0; _i < unit[0].length && n > 0; _i++) {
-      var p = '';
-      for (var j = 0; j < unit[1].length && n > 0; j++) {
-        p = digit[n % 10] + unit[1][j] + p;
-        n = Math.floor(n / 10);
+    money = parseFloat(money);
+    if (money >= maxNum) {
+      console.log('超出最大处理数字');
+      return "";
+    }
+    if (money == 0) {
+      ChineseStr = cnNums[0] + cnIntLast + cnInteger;
+      return ChineseStr;
+    }
+    money = money.toString(); //转换为字符串
+    if (money.indexOf(".") == -1) {
+      IntegerNum = money;
+      DecimalNum = '';
+    } else {
+      parts = money.split(".");
+      IntegerNum = parts[0];
+      DecimalNum = parts[1].substr(0, 4);
+    }
+    if (parseInt(IntegerNum, 10) > 0) {
+      //获取整型部分转换
+      zeroCount = 0;
+      IntLen = IntegerNum.length;
+      for (i = 0; i < IntLen; i++) {
+        n = IntegerNum.substr(i, 1);
+        p = IntLen - i - 1;
+        q = p / 4;
+        m = p % 4;
+        if (n == "0") {
+          zeroCount++;
+        } else {
+          if (zeroCount > 0) {
+            ChineseStr += cnNums[0];
+          }
+          zeroCount = 0; //归零
+          ChineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
+        }
+        if (m == 0 && zeroCount < 4) {
+          ChineseStr += cnIntUnits[q];
+        }
       }
-      s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][_i] + s;
+      ChineseStr += cnIntLast;
+      //整型部分处理完毕
     }
-    return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
+    if (DecimalNum != '') {
+      //小数部分
+      decLen = DecimalNum.length;
+      for (i = 0; i < decLen; i++) {
+        n = DecimalNum.substr(i, 1);
+        if (n != '0') {
+          ChineseStr += cnNums[Number(n)] + cnDecUnits[i];
+        }
+      }
+    }
+    if (ChineseStr == '') {
+      ChineseStr += cnNums[0] + cnIntLast + cnInteger;
+    } else if (DecimalNum == '') {
+      ChineseStr += cnInteger;
+    }
+    return ChineseStr;
   }
 };
